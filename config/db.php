dd18d4bff86bdf2659e6e765183f1bba5eb45f45
Shyn <nguyenthiy1403@gmail.com>
2026-02-14 17:55:03 +0700

    Bán hàng & Đơn hàng & Gói tập
    - Hoàn thiện sơ bộ CRUD: Categories, Products, Packages, member_packages, orders, order_items
    
    Bảng order_item và cart_item CẦN CÓ product_id (Sẽ thêm sau)
    
    Hoàn thiện PHP kiểm tra các bảng trên
    
    Kiểm tra config/db.php trước khi test

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "123456";
+$password = "";
 $dbname = "gym_management";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
